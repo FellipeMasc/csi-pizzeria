2082c6ad2bc25e6309d690bd38e9a7db635e069d
Fellipe <fellipe_sdp@hotmail.com>
2024-06-19 20:48:37 -0300

    fix(main): changes in name

---
+++ PizzaShop.py
@@ -0,0 +1,28 @@
+from Pizza import *
+
+class PizzaShop():
+	# Exemplo:
+	#1. Pizza de frango catupiry
+	pizza_frango_catupiry = BasePizza()
+	pizza_frango_catupiry = CatupiryCheese(pizza_frango_catupiry)
+	pizza_frango_catupiry = Chicken(pizza_frango_catupiry)
+	pizza_frango_catupiry = Olives(pizza_frango_catupiry)
+
+	print("Pizza de frango catupiry:")
+	print(f"Ingredientes: {pizza_frango_catupiry.get_description()}")
+	print(f"Custo: {pizza_frango_catupiry.get_cost()}")
+
+
+	#2. Pizza de calabresa
+	pizza_calabresa = BasePizza()
+	pizza_calabresa = MozzarellaCheese(pizza_calabresa)
+	pizza_calabresa = Onions(pizza_calabresa)
+	pizza_calabresa = Pepperoni(pizza_calabresa)
+	pizza_calabresa = Olives(pizza_calabresa)
+
+	print("Pizza de calabresa:")
+	print(f"Ingredientes: {pizza_calabresa.get_description()}")
+	print(f"Custo: {pizza_calabresa.get_cost()}")
+ 
+ 
+pizza_shop = PizzaShop()
